95dbe23d9ab78328e9ee71b39595cd5e3f368efc
dasxtes <140609752+dasxtes@users.noreply.github.com>
2025-03-24 11:43:07 +0000

    yj

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ document.addEventListener('DOMContentLoaded', function () {
     let tamanhoAtualFonte = 1;
     const aumentarFonteBotao = document.getElementById('aumentar-fonte');
     aumentarFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte += 0, 1;
+        tamanhoAtualFonte += 0,1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
 
-const diminuirFonteBotao = document.getElementById('aumentar-fonte');
-diminuirFonteBotaoFonteBotao.addEventListener('click', function () {
-    tamanhoAtualFonte -= 0, 1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-})
+    const diminuirFonteBotao = document.getElementById('aumentar-fonte');
+    diminuirFonteBotaoFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0,1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
 
 
 })
